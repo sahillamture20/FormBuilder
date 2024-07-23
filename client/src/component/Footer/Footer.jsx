4d05e12fd2b311d5828be4db54f4bb34c5680d3a
Sahil Lamture <76159336+sahillamture20@users.noreply.github.com>
2024-07-23 15:26:56 +0000

    Add footer

--- client/src/component/Footer/Footer.jsx
+++ client/src/component/Footer/Footer.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
-
+import '../Footer/style.css'
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='part1'>
+        <ul>
+          <li>Made with <i class="ri-heart-fill"></i> by</li>
+          <li>@cuvette</li>
+        </ul>
+      </div>
+      <div className='part2'>
+        <ul>
+          <li>Status <i class="ri-external-link-line"></i></li>
+          <li>Dcumentation <i class="ri-external-link-line"></i></li>
+          <li>Roadmap <i class="ri-external-link-line"></i></li>
+          <li>Pricing</li>
+        </ul>
+      </div>
+      <div className='part3'>
+        <ul>
+          <li>Discord <i class="ri-external-link-line"></i></li>
+          <li>Github repository <i class="ri-external-link-line"></i></li>
+          <li>Twitter <i class="ri-external-link-line"></i></li>
+          <li>LinkedIn <i class="ri-external-link-line"></i></li>
+          <li>OSS Friends</li>
+        </ul>
+      </div>
+      <div className='part4'>
+        <ul>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Terms of Services</li>
+          <li>Privacy Policy</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
--- client/src/component/landing/DesktopLanding.jsx
+++ client/src/component/landing/DesktopLanding.jsx
@@ -8,7 +8,7 @@ const DesktopLanding = () => {
     <>
       <Nav />
       <Body />
-      {/* <Footer /> */}
+      <Footer />
     </>
   )
 }
